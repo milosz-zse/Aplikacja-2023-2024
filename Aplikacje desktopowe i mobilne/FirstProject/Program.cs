2ccc57f2ef26f859c22a25d0535e59e212e1b293
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-13 13:55:39 +0200

    Utworzenie projektu

---
+++ Aplikacje desktopowe i mobilne/FirstProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //c++
+            //typ nazwa = wartość;
+            //int liczba = 5;
+            //int x,y;
+
+
+
+
+        }
+    }
+}
