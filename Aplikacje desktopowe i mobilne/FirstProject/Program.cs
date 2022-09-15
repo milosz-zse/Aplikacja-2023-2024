3580a6594788dd8eb76cb33d133afe026f16b4f4
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-15 09:28:07 +0200

    koniec zajęć 15.09.2022

--- Aplikacje desktopowe i mobilne/FirstProject/Program.cs
+++ Aplikacje desktopowe i mobilne/FirstProject/Program.cs
@@ -4,16 +4,35 @@ namespace FirstProject
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
 
             //c++
             //typ nazwa = wartość;
             //int liczba = 5;
             //int x,y;
 
+            string imie = "Jan";
+            string nazwisko = "Kowalski";
+            //nazwisko.Length
 
+            Console.WriteLine("Witaj " + imie + " " + nazwisko + " tutaj");
+            Console.WriteLine("Witaj {1} {0} tutaj",imie, nazwisko);
+            Console.WriteLine($"Witaj {imie} xxxx {nazwisko} tutaj");
+
+
+            Console.WriteLine("Prędkość to km\\h");
+            Console.WriteLine(@"Prędkość to km\h");
+
+            int liczbaCalkowita = 5;
+            double liczbaRzeczywista = 5.6;
+
+   
 
 
         }
