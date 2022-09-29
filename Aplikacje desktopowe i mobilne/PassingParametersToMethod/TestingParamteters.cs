aa1ebb77fd3c8d46bc79c871b838f08bbb2d0dab
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-09-29 13:18:53 +0200

    koniec lekcji

--- Aplikacje desktopowe i mobilne/PassingParametersToMethod/TestingParamteters.cs
+++ Aplikacje desktopowe i mobilne/PassingParametersToMethod/TestingParamteters.cs
@@ -10,7 +10,42 @@ namespace PassingParametersToMethod
     {
         public void Test1()
         {
+            byte number = 15;
+            Console.WriteLine("Zmienna number = " + number);
+            //ChangeValue(number);
+            //ChangeValue(6);
+            ChangeValueV2(ref number);
+            //ChangeValueV2(ref 6); BŁAD!!!!!!!!!
+            Console.WriteLine("Zmienna number = " + number);
 
+            int random = GetRandomNumber();
+            GetRandomNumber(out random);
         }
+
+        private void ChangeValue(byte value)
+        {
+            Console.WriteLine("Parametr value = " + value);
+            value = (byte)(value + 10);
+            Console.WriteLine("Parametr value = " + value);
+        }
+
+        private void ChangeValueV2(ref byte value)
+        {
+            Console.WriteLine("Parametr value = " + value);
+            value = (byte)(value + 10);
+            Console.WriteLine("Parametr value = " + value);
+        }
+
+        private int GetRandomNumber()
+        {
+            return new Random().Next();
+        }
+
+        private void GetRandomNumber(out int value)
+        {
+            //value = new Random().Next();
+            Console.WriteLine("Parametr value = " + value);
+        }
+
     }
 }
