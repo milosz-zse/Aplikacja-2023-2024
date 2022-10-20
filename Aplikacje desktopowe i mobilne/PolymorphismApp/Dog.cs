67cf3c66ce9356d60b32bce0282fc9827ccef4eb
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-20 13:16:58 +0200

    koniec lekcji

---
+++ Aplikacje desktopowe i mobilne/PolymorphismApp/Dog.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismApp
+{
+
+    class Dog
+    {
+        private string name;
+        private byte age;
+
+        public Dog()
+        {
+            name = "Azor";
+            age = 10;
+        }
+
+        public Dog(string name, byte age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Dog(Dog dogToCopy)
+        {
+            name = dogToCopy.name;
+            age = dogToCopy.age;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("Pies o imieniu " + name + " ma " + age + " lat");
+            Console.WriteLine("***********************");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Pies o imieniu " + name + " szczeka");
+        }
+
+        public void ChasePostman()
+        {
+            Console.WriteLine("Pies o imieniu " + name + " goni listonosza");
+        }
+    }
+}
--- Aplikacje desktopowe i mobilne/PolymorphismApp/Program.cs
+++ Aplikacje desktopowe i mobilne/PolymorphismApp/Program.cs
@@ -6,7 +6,10 @@ namespace PolymorphismApp
     {
         static void Main(string[] args)
         {
-            
+            Dog firstDog = new Dog();
+            Dog secondDog = new Dog("Burek", 5);
+
+            Dog thirdDog = new Dog(secondDog);
         }
     }
 }
