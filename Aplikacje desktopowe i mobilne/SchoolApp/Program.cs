c71908cb54695f977588b3a64b622690fbed4ca2
IronLittleFox <lukasz.felisek@gmail.com>
2023-03-30 14:24:13 +0200

    koniec lekcji

---
+++ Aplikacje desktopowe i mobilne/SchoolApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using SchoolApp.Database;
+using System;
+
+namespace SchoolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolDatabaseContext schoolDatabaseContext = new SchoolDatabaseContext();
+
+        }
+    }
+}
