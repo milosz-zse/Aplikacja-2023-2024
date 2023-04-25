2d215d141d90c54e2057bbb2e1ac6caf36631bca
IronLittleFox <lukasz.felisek@gmail.com>
2023-04-25 11:39:47 +0200

    koniec lekcji

--- Aplikacje desktopowe i mobilne/SchoolApp/School.cs
+++ Aplikacje desktopowe i mobilne/SchoolApp/School.cs
@@ -1,9 +1,19 @@
 ﻿using SchoolApp.Database;
+using SchoolApp.Database.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+/*
+    CRUD
+    C - create  - insert into
+    R - read    - select
+    U - update  - update
+    D - delete  - delete
+  
+ */
 
 namespace SchoolApp
 {
@@ -19,7 +29,16 @@ namespace SchoolApp
 
                 int chosenOption = GetOption();
 
-
+                switch (chosenOption)
+                {
+                    case 1:
+                        CreateNewSchoolClass();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        break;
+                }
             }
         }
 
@@ -42,9 +61,37 @@ namespace SchoolApp
         {
             Console.Clear();
             Console.WriteLine("Wybierz opcje:");
-            Console.WriteLine("1.  ");
+            Console.WriteLine("1. Dodaj nową klasę w szkole");
+            Console.WriteLine("2. Wyświetl wszystkie klasy w szkole");
 
             Console.WriteLine("0. Koniec programu");
+        }
+
+        private void CreateNewSchoolClass()
+        {
+            Console.WriteLine("Podaj nazwę klasy:");
+            string className = Console.ReadLine();
+
+            SchoolClass schoolClass = new SchoolClass()
+            {
+                Name = className
+            };
+            schoolDatabaseContext.SchoolClasses.Add(schoolClass);
+            schoolDatabaseContext.SaveChanges();
+        }
+
+        private void ReadAllSchoolClasses()
+        {
+            /*
+             select *
+               from nazwaBazy.SchoolClasses
+             */
+
+            foreach (SchoolClass schoolClass in schoolDatabaseContext.SchoolClasses)
+            {
+                Console.WriteLine("Id: " + schoolClass.Id + " Nazwa: " + schoolClass.Name);
+            }
+            Console.ReadKey();
         }
     }
 }
