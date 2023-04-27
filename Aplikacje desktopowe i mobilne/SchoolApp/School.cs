aab7cfac4a6dc4f4a715b6a3e5324d32ecba1382
IronLittleFox <lukasz.felisek@gmail.com>
2023-04-27 13:46:48 +0200

    koniec lekcji

--- Aplikacje desktopowe i mobilne/SchoolApp/School.cs
+++ Aplikacje desktopowe i mobilne/SchoolApp/School.cs
@@ -34,6 +34,12 @@ namespace SchoolApp
                     case 1:
                         CreateNewSchoolClass();
                         break;
+                    case 2:
+                        ReadAllSchoolClasses();
+                        break;
+                    case 3:
+                        DeleteSchoolClass();
+                        break;
                     case 0:
                         return;
                     default:
@@ -63,6 +69,7 @@ namespace SchoolApp
             Console.WriteLine("Wybierz opcje:");
             Console.WriteLine("1. Dodaj nową klasę w szkole");
             Console.WriteLine("2. Wyświetl wszystkie klasy w szkole");
+            Console.WriteLine("3. Skasuj klasę w szkole");
 
             Console.WriteLine("0. Koniec programu");
         }
@@ -91,6 +98,34 @@ namespace SchoolApp
             {
                 Console.WriteLine("Id: " + schoolClass.Id + " Nazwa: " + schoolClass.Name);
             }
+            Console.ReadKey();
+        }
+
+        private void DeleteSchoolClass()
+        {
+            Console.WriteLine("Podaj id klasy do skasowania");
+            if (!int.TryParse(Console.ReadLine(), out int idSchoolClassToDelete))
+            {
+                Console.WriteLine("Nie podałeś poprawnej danej");
+                Console.ReadKey();
+                return;
+            }
+
+            /*
+             delete from SchoolClasses
+             where id = idSchoolClassToDelete
+             */
+            SchoolClass schoolClassToDelete = schoolDatabaseContext
+                .SchoolClasses
+                .FirstOrDefault(sc => sc.Id == idSchoolClassToDelete);
+            if (schoolClassToDelete != null)
+            {
+                schoolDatabaseContext.SchoolClasses.Remove(schoolClassToDelete);
+                schoolDatabaseContext.SaveChanges();
+                Console.WriteLine("Kasowanie klasy zakończone sukcesem");
+            }
+            else
+                Console.WriteLine("Brak takiej klasy w bazie");
             Console.ReadKey();
         }
     }
